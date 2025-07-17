3dba27af44893d93bbff06db575efd11ecd88332
Valentina Velazquez <136092441+valuvel17@users.noreply.github.com>
2025-07-16 23:19:48 -0300

    Modify Fanta font and upload main text

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,33 @@
-export default function Layout() {
-    return (
-        <></>
-    )
+export default function Layout(props) {
+  const { children } = props;
+
+  const header = (
+    <header>
+      <h1 className="text-gradient">The Program</h1>
+      <p>
+        <strong>30 simple Workouts Program</strong>
+      </p>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <p>
+        Built by{" "}
+        <a href="" target="_blank">
+          Valentina Velázquez
+        </a>{" "}
+        <br />
+        Styled with FantaCSS
+      </p>
+    </footer>
+  );
+
+  return (
+    <>
+      {header}
+      {children}
+      {footer}
+    </>
+  );
 }
